4f42b692ec18496655c61cc3968f9f27ea27ec14
Михаил <mxoilslukin@gmail.com>
2023-07-13 14:45:48 +0500

    dbg

--- studyBackend/studyBackend/settings.py
+++ studyBackend/studyBackend/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'drf_spectacular',
